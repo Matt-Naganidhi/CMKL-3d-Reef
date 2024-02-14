25deab2b29ae3d4b00418a5ede4570446f82a309
Sarutch Supaibulipat (Uni) <112068235+POKerPONGus@users.noreply.github.com>
2024-02-14 14:57:24 +0700

    Added camera rotation, pan, and zoom.

--- webApp/prototype/renderer/client/renderer.js
+++ webApp/prototype/renderer/client/renderer.js
@@ -1,5 +1,5 @@
-// import * as THREE from "https://cdn.jsdelivr.net/npm/three@latest/build/three.module.js"
-import * as THREE from "./three.module.js"
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 100)
@@ -18,8 +18,11 @@ const torus = new THREE.Mesh(geo, mat);
 
 scene.add(torus);
 
+const controls = new OrbitControls(camera, renderer.domElement);
+
 function animate() {
     requestAnimationFrame(animate);
+    controls.update();
     renderer.render(scene, camera);
 }
 
