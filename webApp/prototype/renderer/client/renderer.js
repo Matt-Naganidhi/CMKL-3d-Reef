128aefbdc9f834f9deea5feb9f3fd0452702c21c
Sarutch Supaibulipat (Uni) <112068235+POKerPONGus@users.noreply.github.com>
2024-02-15 21:14:29 +0700

    Adding mesh loader. Currently from static files.

--- webApp/prototype/renderer/client/renderer.js
+++ webApp/prototype/renderer/client/renderer.js
@@ -1,25 +1,43 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader.js";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 100)
+// 3D construct for testing
+const MESH_PATH = "birdBushMesh/mesh.obj";
+const MAT_PATH = "birdBushMesh/mesh.mtl";
+
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector("#display")
 });
-
+// Display size = whole window. Not dynamic
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-camera.position.setZ(30);
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 100)
+camera.position.setZ(5);
 
-const geo = new THREE.TorusGeometry(10, 3, 16, 100);
-const mat = new THREE.MeshBasicMaterial({color: 0xff6347, wireframe: true});
-const torus = new THREE.Mesh(geo, mat);
-
-scene.add(torus);
-
+// Camera movement controls
 const controls = new OrbitControls(camera, renderer.domElement);
 
+const point_light = new THREE.PointLight(0xffffff, 100);
+const ambient_light = new THREE.AmbientLight(0xffffff, 0.5);
+point_light.position.set(5, 5, 5);
+scene.add(point_light, ambient_light);
+
+// Load 3D construct file
+const mat_loader = new MTLLoader();
+const geo_loader = new OBJLoader();
+mat_loader.load(MAT_PATH, (material) => {
+    material.preload();
+    geo_loader.setMaterials(material);
+    geo_loader.load(MESH_PATH, (mesh) => {
+        scene.add(mesh);
+    });
+});
+
+// Update loop
 function animate() {
     requestAnimationFrame(animate);
     controls.update();
