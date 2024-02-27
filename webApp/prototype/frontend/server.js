4c1217312ab03752aeba27a84da9fcfded74b578
Sarutch Supaibulipat (Uni) <112068235+POKerPONGus@users.noreply.github.com>
2024-02-27 21:43:48 +0700

    refactor: Reorganized frontend.
        Changes:
        - Renamed: renderer/ -> frontend/
        - Moved: Ten's html pages -> frontend/templates
        - Created: Copy of ten's html page in frontend/client/ and seperated the css and js
        - Renamed: other files in frontend/client/

---
+++ webApp/prototype/frontend/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const path = require("path");
+
+const HOST = 'localhost';
+const PORT = 3000;
+const CLIENT_PATH = "client";
+
+const server = express();
+server.listen(PORT, () => console.log("listening..."))
+server.use(express.static(path.join(__dirname, CLIENT_PATH)));
--- webApp/prototype/renderer/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-
-const HOST = 'localhost';
-const PORT = 3000;
-
-const server = express();
-server.listen(PORT, () => console.log("listening..."))
-server.use(express.static("client"));
