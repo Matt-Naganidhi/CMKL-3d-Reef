21e2946ce5e51f8dc1587e7c85d1c48cf6b5222c
SFocusP <sphromsi@cmkl.ac.th>
2024-03-05 16:55:19 +0700

    Video upload and labels
    Video upload now works, and labels are now stored in the db

--- django/coralReef/myapp/models.py
+++ django/coralReef/myapp/models.py
@@ -5,7 +5,7 @@ import os
 class TrainingJob(models.Model):
     job_id = models.CharField(max_length=255, unique=True)  # Unique identifier for each job
     file_path = models.FileField(upload_to='uploaded_files/')  # Storing files in the 'uploaded_files' directory within MEDIA_ROOT
-    success_status = models.BooleanField(default=False)  # Status of the job (True if successful, False otherwise)
+    success_status = models.CharField(max_length=50, default='pending')  # Status of the job (True if successful, False otherwise)
     timestamp = models.DateTimeField(auto_now_add=True)  # Job creation time
 
     def __str__(self):
