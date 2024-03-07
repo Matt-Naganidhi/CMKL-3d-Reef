d8df3ee82512f6fd2764cca7e9abae4bc8058378
Sarutch Supaibulipat (Uni) <112068235+POKerPONGus@users.noreply.github.com>
2024-03-07 16:21:27 +0700

    test: sending the ply through a request instead of being in the client side directory.

--- webApp/prototype/frontend/server.js
+++ webApp/prototype/frontend/server.js
@@ -3,8 +3,17 @@ const path = require("path");
 
 const HOST = "localhost";
 const PORT = 3000;
-const CLIENT_PATH = "client";
+const CLIENT_PATH = path.join(__dirname, "client");
 
 const server = express();
 server.listen(PORT, () => console.log("listening..."))
-server.use(express.static(path.join(__dirname, CLIENT_PATH)));
+server.use(express.static(CLIENT_PATH));
+
+server.get("/", (req, res) => {
+    res.sendFile(path.join(CLIENT_PATH, "upload.html"));
+});
+
+server.get("/view", (req, res) =>{
+    res.sendFile(filePath, path.join(__dirname, "goat_skull_ply_1", "Goat skull.ply"));
+    res.on("error", err => console.log(err));
+});
